c710100536375f1a8b973b0a0374d53dfc26eb4f
KushnirDmytro <kushnir.dmytro.93@gmail.com>
2019-04-05 11:37:26 +0300

    Added ARIMA model example added normalisation from sklearn added reading from files requirements.txt updated simulation update is cosmetic

---
+++ load_matlab_timedata.py
@@ -0,0 +1,10 @@
+import numpy as np
+import h5py
+
+
+def get_data_from(data_file_name):
+    outfile = h5py.File(data_file_name, 'r')
+    print(outfile.keys())
+    data = outfile['ans']
+    return data
+
